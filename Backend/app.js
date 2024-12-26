0dec6c3c2d1f29ae1b7bc094bec41d8c1516cf96
durgeshkushwaha23 <durgeshkushwaha415@gmail.com>
2024-12-26 22:06:46 +0530

    register completed

--- Backend/app.js
+++ Backend/app.js
@@ -6,13 +6,16 @@ import morgan from 'morgan';
 import userRoutes from './routes/user.route.js';
 import cookieParser from 'cookie-parser';
 import redisClient from './services/redis.service.js';
+import Cors from 'cors';
+
+
 connect();
 const app = express();
 
 
 
 
-
+app.use(Cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import Home from "../screens/Home";
+import Register from "../screens/Register";
+import Login from "../screens/Login";
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="/login" element={<div>g</div>} />
-        <Route path="/regiter" element={<div>ef</div>} />
+        <Route path="/users/login" element={<Login/>} />
+        <Route path="/users/register" element={<Register/>} />
 
       </Routes>
     </BrowserRouter>
